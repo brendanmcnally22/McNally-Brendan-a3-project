99ac3512c1c31067f2a15eb396fcfb06b6fe55b8
brendanmcnally22 <brendanmcnally295@gmail.com>
2025-03-08 13:14:00 -0500

    Add Code - Added Hazard Class

---
+++ McNally-Brendan-a3-project/McNally-Brendan-a3-project/Hazard.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+namespace MohawkGame2D
+{
+    public class Hazard
+    {
+        public Vector2 Position { get; private set; }
+        public Vector2 Size { get; private set; }
+
+        public Hazard(Vector2 position, Vector2 size)
+        {
+            Position = position;
+            Size = size;
+        }
+
+        public void DrawHazard()
+        {
+            Draw.FillColor = Color.Magenta;
+            Draw.Rectangle(Position, Size);
+        }
+    }
+}
